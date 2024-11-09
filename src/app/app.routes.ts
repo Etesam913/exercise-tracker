7a890ee991daf1727b725d7fa1ffbcbc4d989c04
etesam <etesamansari13@gmail.com>
2024-11-08 20:09:42 -0500

    :mag: Added 404 error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { ExerciseCreatorComponent } from "./routes/exercise-creator/exercise-creator.component";
 import { HomeComponent } from "./routes/home/home.component";
+import { PageNotFoundComponent } from "./routes/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent, title: "Home" },
@@ -8,5 +9,9 @@ export const routes: Routes = [
     path: "exercise-creator",
     component: ExerciseCreatorComponent,
     title: "Exercise Creator",
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
   },
 ];
